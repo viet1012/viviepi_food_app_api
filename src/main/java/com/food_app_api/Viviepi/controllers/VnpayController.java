97ffa65a2595ab48ba7b9c8ff5efe283c829fb30
vietta1012 <viettta1012@gmail.com>
2024-03-27 12:52:28 +0800

    Update VnpayController.java
    Them

--- src/main/java/com/food_app_api/Viviepi/controllers/VnpayController.java
+++ src/main/java/com/food_app_api/Viviepi/controllers/VnpayController.java
@@ -64,76 +64,76 @@ public class VnpayController {
             return ResponseEntity.badRequest().body("Invalid billId.");
         }
     }
-    @GetMapping("pay1")
-    public String getPay1(@RequestParam("price") long price,@RequestParam("id") long
-            billId) throws UnsupportedEncodingException {
-
-        String vnp_Version = "2.1.0";
-        String vnp_Command = "pay";
-        String orderType = "other";
-        long amount = price *100;
-        String bankCode = "NCB";
-
-        String vnp_TxnRef = VnpayConfig.getRandomNumber(8);
-        String vnp_IpAddr = "127.0.0.1";
-
-        String vnp_TmnCode = VnpayConfig.vnp_TmnCode;
-
-        Map<String, String> vnp_Params = new HashMap<>();
-        vnp_Params.put("vnp_Version", vnp_Version);
-        vnp_Params.put("vnp_Command", vnp_Command);
-        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
-        vnp_Params.put("vnp_Amount", String.valueOf(amount));
-        vnp_Params.put("vnp_CurrCode", "VND");
-
-        vnp_Params.put("vnp_BankCode", bankCode);
-        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
-        vnp_Params.put("vnp_OrderType", orderType);
-
-        vnp_Params.put("vnp_Locale", "vn");
-        vnp_Params.put("vnp_ReturnUrl", VnpayConfig.vnp_ReturnUrl + "?billId=" + String.valueOf(billId));
-        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
-
-        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-        String vnp_CreateDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
-        cld.add(Calendar.MINUTE, 15);
-        String vnp_ExpireDate = formatter.format(cld.getTime());
-        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
-
-        List fieldNames = new ArrayList(vnp_Params.keySet());
-        Collections.sort(fieldNames);
-        StringBuilder hashData = new StringBuilder();
-        StringBuilder query = new StringBuilder();
-        Iterator itr = fieldNames.iterator();
-        while (itr.hasNext()) {
-            String fieldName = (String) itr.next();
-            String fieldValue = (String) vnp_Params.get(fieldName);
-            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                //Build hash data
-                hashData.append(fieldName);
-                hashData.append('=');
-                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                //Build query
-                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-                query.append('=');
-                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                if (itr.hasNext()) {
-                    query.append('&');
-                    hashData.append('&');
-                }
-            }
-        }
-        String queryUrl = query.toString();
-        String vnp_SecureHash = VnpayConfig.hmacSHA512(VnpayConfig.secretKey, hashData.toString());
-        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
-        String paymentUrl = VnpayConfig.vnp_PayUrl + "?" + queryUrl;
-
-        return paymentUrl;
-    }
+//    @GetMapping("pay1")
+//    public String getPay1(@RequestParam("price") long price,@RequestParam("id") long
+//            billId) throws UnsupportedEncodingException {
+//
+//        String vnp_Version = "2.1.0";
+//        String vnp_Command = "pay";
+//        String orderType = "other";
+//        long amount = price *100;
+//        String bankCode = "NCB";
+//
+//        String vnp_TxnRef = VnpayConfig.getRandomNumber(8);
+//        String vnp_IpAddr = "127.0.0.1";
+//
+//        String vnp_TmnCode = VnpayConfig.vnp_TmnCode;
+//
+//        Map<String, String> vnp_Params = new HashMap<>();
+//        vnp_Params.put("vnp_Version", vnp_Version);
+//        vnp_Params.put("vnp_Command", vnp_Command);
+//        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+//        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+//        vnp_Params.put("vnp_CurrCode", "VND");
+//
+//        vnp_Params.put("vnp_BankCode", bankCode);
+//        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+//        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+//        vnp_Params.put("vnp_OrderType", orderType);
+//
+//        vnp_Params.put("vnp_Locale", "vn");
+//        vnp_Params.put("vnp_ReturnUrl", VnpayConfig.vnp_ReturnUrl + "?billId=" + String.valueOf(billId));
+//        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+//
+//        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+//        String vnp_CreateDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+//
+//        cld.add(Calendar.MINUTE, 15);
+//        String vnp_ExpireDate = formatter.format(cld.getTime());
+//        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+//
+//        List fieldNames = new ArrayList(vnp_Params.keySet());
+//        Collections.sort(fieldNames);
+//        StringBuilder hashData = new StringBuilder();
+//        StringBuilder query = new StringBuilder();
+//        Iterator itr = fieldNames.iterator();
+//        while (itr.hasNext()) {
+//            String fieldName = (String) itr.next();
+//            String fieldValue = (String) vnp_Params.get(fieldName);
+//            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+//                //Build hash data
+//                hashData.append(fieldName);
+//                hashData.append('=');
+//                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                //Build query
+//                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+//                query.append('=');
+//                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+//                if (itr.hasNext()) {
+//                    query.append('&');
+//                    hashData.append('&');
+//                }
+//            }
+//        }
+//        String queryUrl = query.toString();
+//        String vnp_SecureHash = VnpayConfig.hmacSHA512(VnpayConfig.secretKey, hashData.toString());
+//        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+//        String paymentUrl = VnpayConfig.vnp_PayUrl + "?" + queryUrl;
+//
+//        return paymentUrl;
+//    }
     @PostMapping("pay")
     public String getPay(@RequestBody BillDTO billRequest ,@RequestParam(name = "codeVoucher", required = false) String codeVoucher,
                          @RequestParam(name = "Authorization") String token) throws UnsupportedEncodingException {
