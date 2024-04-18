755d66de500d8e306f1a878ff2aefa9a1bbcb52a
Viet Ta <viettta1012@gmail.com>
2024-04-18 23:39:40 +0700

    update user

--- src/main/java/com/food_app_api/Viviepi/mapper/UserMapper.java
+++ src/main/java/com/food_app_api/Viviepi/mapper/UserMapper.java
@@ -14,8 +14,7 @@ import java.util.stream.Collectors;
 @Component
 public class UserMapper {
     public User toUser(User user , UserDTO userDTO) {
-        user.setId(userDTO.getId());
-        user.setAvtUrl(userDTO.getAvtUrl());
+        user.setAvtUrl(userDTO.getAvtUrl() != null ? userDTO.getAvtUrl() : null);
         user.setFullname(userDTO.getFullName());
         user.setPhoneNumber(userDTO.getPhone());
         return user;
